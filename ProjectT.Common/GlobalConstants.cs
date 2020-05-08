9ec87f81fa02eef4d03e6bd9c3fe6ff6125d4201
Enes <losthell92@gmail.com>
2020-05-09 02:53:29 +0300

    {STABLE} Architecture initialize!

---
+++ ProjectT.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectT.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "ProjectT";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
---
+++ Services/ProjectT.Services.Mapping/IHaveCustomMappings.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectT.Services.Mapping
+{
+    using AutoMapper;
+
+    public interface IHaveCustomMappings
+    {
+        void CreateMappings(IProfileExpression configuration);
+    }
+}
