01569a4d8f412a05ab7fee128ee0998579d6326a
Enes <losthell92@gmail.com>
2020-05-10 01:18:55 +0300

    Change table Cart!

---
+++ Data/ProjectT.Data/Migrations/20200509221319_ChangeRowInCart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectT.Data.Migrations
+{
+    public partial class ChangeRowInCart : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created_On",
+                table: "Carts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created_On",
+                table: "Carts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
