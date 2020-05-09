b5dcba7656c873744776b7755a88c2b84fc87e9b
Enes <losthell92@gmail.com>
2020-05-10 02:08:07 +0300

    Added Seeders for User and Product

--- ProjectT.Common/GlobalConstants.cs
+++ ProjectT.Common/GlobalConstants.cs
@@ -5,5 +5,15 @@
         public const string SystemName = "ProjectT";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const string UserRoleName = "User";
+
+        public const string AdministratorUsername = "admina";
+
+        public const string AdministratorPassword = "admina";
+
+        public const string UserUsername = "userche";
+
+        public const string UserPassword = "userche";
     }
 }
