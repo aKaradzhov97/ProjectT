4ae9bdcc638b97383e7d9b47f3a14f2946fbc442
Enes <losthell92@gmail.com>
2020-05-10 02:13:36 +0300

    Added ViewModels Login,Register and Products

---
+++ Web/ProjectT.Web.ViewModels/Products/ProductsInputViewModel.cs
@@ -0,0 +1,37 @@
+﻿namespace ProjectT.Web.ViewModels.Products
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using ProjectT.Data.Models;
+    using ProjectT.Services.Mapping;
+
+    public class ProductsInputViewModel:IMapFrom<Product>
+    {
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Product name is required!")]
+        [StringLength(200, ErrorMessage = "Product name must be between 4 and 200 characters long!", MinimumLength = 4)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Product description is required!")]
+        [StringLength(4000, ErrorMessage = "Product description must be between 10 and 4000 characters long!", MinimumLength = 10)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Image url is required!")]
+        public string Image { get; set; }
+
+        [Required(ErrorMessage = "Product price is required!")]
+        [Range(0.01, 100000.00, ErrorMessage = "Product price must be bweteen 0.01 and 100 000.00")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Product quantity is required!")]
+        [Range(1, 100000, ErrorMessage = "Product quantity must be between 1 and 100 000")]
+        public int Quantity { get; set; }
+
+        [Required]
+        public DateTime Created_On { get; set; }
+
+        public int SellCount { get; set; }
+    }
+}
---
+++ Web/ProjectT.Web.ViewModels/Users/RegisterInputViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace ProjectT.Web.ViewModels.Users
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class RegisterInputViewModel
+    {
+        [Required(ErrorMessage = "Username is Required.")]
+        [Display(Name = "Username")]
+        [StringLength(30, ErrorMessage = "Username must be between 5 and 30 characters long.", MinimumLength = 5)]
+        [RegularExpression(@"^[A-z0-9]{5,30}$")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email address is Required.")]
+        [RegularExpression(@"^[A-z0-9\.]{3,30}\@[A-z]{3,11}\.[A-z]{2,7}$", ErrorMessage = "Invalid Email address!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone number is Required.")]
+        [Phone]
+        [RegularExpression("^[0]{1}[8]{1}[7-9]{1}[0-9]{7}$", ErrorMessage = "Your phone must be 10 digits and starting 087/088/089...")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Password is Required.")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [StringLength(30, ErrorMessage = "Password must be between 6 and 30 characters long", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Repeat Password is Required.")]
+        [Display(Name = "Repeat password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords mismatch!")]
+        public string RepeatPassword { get; set; }
+    }
+}
