5deaaa0da806f4e52f16d55ab7be8709ab9f264b
Enes <losthell92@gmail.com>
2020-05-28 10:46:38 +0200

    Changed DB name and Products Seeder

--- Data/ProjectT.Data/Seeding/ProductsSeeder.cs
+++ Data/ProjectT.Data/Seeding/ProductsSeeder.cs
@@ -20,14 +20,14 @@
                     string size, int sellCount)>
                 {
                     (
-                        "Mens T-Shirt",
-                        "Luftfilter, Ölfilter, Zündkerzen - die Marke *Champion* gehört zu den ganz großen der Szene.",
-                        "https://cdn1.louis.de/dynamic/articles/o_pad,w_1800,h_1800,ha_center,va_center,c_fff/1c.01.32.D3ChampionTShirtWeiss21859427019.JPG",
+                        "Levi's® Print T-shirt",
+                        "Material & care Outer fabric material: 100% cotton Fabric: Jersey Care instructions: Machine wash at 30°C",
+                        "https://img01.ztat.net/article/LE/22/2O/00/RQ/11/LE222O00R-Q11@20.jpg?imwidth=1800",
                         new List<string>
                         {
-                            "https://cdn1.louis.de/dynamic/articles/o_pad,w_1800,h_1800,ha_center,va_center,c_fff/1c.01.32.D3ChampionTShirtWeiss21859427019.JPG",
-                            "https://cdn1.louis.de/dynamic/articles/o_pad,w_1800,h_1800,ha_center,va_center,c_fff/1c.01.32.D3ChampionTShirtWeiss21859427019.JPG",
-                            "https://cdn1.louis.de/dynamic/articles/o_pad,w_1800,h_1800,ha_center,va_center,c_fff/1c.01.32.D3ChampionTShirtWeiss21859427019.JPG",
+                            "https://img01.ztat.net/article/LE/22/2O/00/RQ/11/LE222O00R-Q11@21.jpg?imwidth=1800",
+                            "https://img01.ztat.net/article/LE/22/2O/00/RQ/11/LE222O00R-Q11@19.jpg?imwidth=1800",
+                            "https://img01.ztat.net/article/LE/22/2O/00/RQ/11/LE222O00R-Q11@17.jpg?imwidth=1800&filter=packshot",
                         },
                         22.50m,
                         10,
@@ -35,14 +35,14 @@
                         22
                     ),
                     (
-                        "Mens T-Shirt",
-                        "Luftfilter, Ölfilter, Zündkerzen - die Marke *Champion* gehört zu den ganz großen der Szene.",
-                        "https://cdn03.plentymarkets.com/j1rx4udxin3f/item/images/1601/full/Schwarz.jpg",
+                        "Karl Kani SIGNATURE RINGER TEE - Print T-shirt",
+                        "Material & care Outer fabric material: 50% polyester, 50% cotton Fabric: Jersey Care instructions: Do not tumble dry, machine wash at 30°C",
+                        "https://img01.ztat.net/article/KK/12/2O/02/2C/11/KK122O022-C11@4.jpg?imwidth=156",
                         new List<string>
                         {
-                            "https://cdn03.plentymarkets.com/j1rx4udxin3f/item/images/1601/full/Schwarz.jpg",
-                            "https://cdn03.plentymarkets.com/j1rx4udxin3f/item/images/1601/full/Schwarz.jpg",
-                            "https://cdn03.plentymarkets.com/j1rx4udxin3f/item/images/1601/full/Schwarz.jpg",
+                            "https://img01.ztat.net/article/KK/12/2O/02/2C/11/KK122O022-C11@9.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/KK/12/2O/02/2C/11/KK122O022-C11@10.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/KK/12/2O/02/2C/11/KK122O022-C11@11.jpg?imwidth=156",
                         },
                         33.50m,
                         10,
@@ -50,15 +50,14 @@
                         10
                     ),
                     (
-                        "Mens T-Shirt - Nike",
-                        "Luftfilter, Ölfilter, Zündkerzen - die Marke *Champion* gehört zu den ganz großen der Szene.",
-                        "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto/e8vqdl1xb7maontxnbw2/sportswear-t-shirt-fur-babys-WLlSQR.jpg",
+                        "Levi's®  Print T-shirt",
+                        "Material & care Outer fabric material: 100% cotton Fabric: Jersey Care instructions: Machine wash at 30°C",
+                        "https://img01.ztat.net/article/LE/22/2O/00/TA/11/LE222O00T-A11@17.jpg?imwidth=156",
                         new List<string>
                         {
-                            "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto/e8vqdl1xb7maontxnbw2/sportswear-t-shirt-fur-babys-WLlSQR.jpg",
-                            "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto/e8vqdl1xb7maontxnbw2/sportswear-t-shirt-fur-babys-WLlSQR.jpg",
-                            "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto/e8vqdl1xb7maontxnbw2/sportswear-t-shirt-fur-babys-WLlSQR.jpg",
-                            "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto/e8vqdl1xb7maontxnbw2/sportswear-t-shirt-fur-babys-WLlSQR.jpg",
+                            "https://img01.ztat.net/article/LE/22/2O/00/TA/11/LE222O00T-A11@18.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/LE/22/2O/00/TA/11/LE222O00T-A11@15.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/LE/22/2O/00/TA/11/LE222O00T-A11@14.1.jpg?imwidth=156&filter=packshot",
                         },
                         47.59m,
                         20,
@@ -66,15 +65,15 @@
                         0
                     ),
                     (
-                        "Mens T-Shirt Puma New Collection",
-                        "Luftfilter, Ölfilter, Zündkerzen - die Marke *Champion* gehört zu den ganz großen der Szene.",
-                        "https://kickz.akamaized.net/de/media/images/p/600/puma-Archive_Logo_T_Shirt-Puma_White-1.jpg",
+                        "Even&Odd 2 PACK - Print T-shirt",
+                        "Material & care Outer fabric material: 95% cotton, 5% elastane Fabric: Jersey Care instructions: Do not tumble dry, machine wash at 30°C, Machine wash on gentle cycle",
+                        "https://img01.ztat.net/article/EV/42/1D/1D/YQ/11/EV421D1DY-Q11@12.1.jpg?imwidth=156",
                         new List<string>
                         {
-                            "https://kickz.akamaized.net/de/media/images/p/600/puma-Archive_Logo_T_Shirt-Puma_White-1.jpg",
-                            "https://kickz.akamaized.net/de/media/images/p/600/puma-Archive_Logo_T_Shirt-Puma_White-1.jpg",
-                            "https://kickz.akamaized.net/de/media/images/p/600/puma-Archive_Logo_T_Shirt-Puma_White-1.jpg",
-                            "https://kickz.akamaized.net/de/media/images/p/600/puma-Archive_Logo_T_Shirt-Puma_White-1.jpg",
+                            "https://img01.ztat.net/article/EV/42/1D/1D/YQ/11/EV421D1DY-Q11@9.1.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/EV/42/1D/1D/YQ/11/EV421D1DY-Q11@14.1.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/EV/42/1D/1D/YQ/11/EV421D1DY-Q11@11.1.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/EV/42/1D/1D/YQ/11/EV421D1DY-Q11@10.1.jpg?imwidth=156",
                         },
                         44.79m,
                         20,
@@ -82,15 +81,14 @@
                         15
                     ),
                     (
-                        "Womens T-Shirt New Collection (2020)",
-                        "Luftfilter, Ölfilter, Zündkerzen - die Marke *Champion* gehört zu den ganz großen der Szene.",
-                        "https://images-na.ssl-images-amazon.com/images/I/81hLKdZYpAL._AC_UY1000_.jpg",
+                        "Protest  SET - Bikini",
+                        "Outer fabric material: 80% polyamide, 20% elastane Bottom part material: 80% polyamide, 20% elastaneTop part material: 80% polyamide, 20% elastane Filling: 100% polyester  Lining: 100% polyester Care instructions: Do not tumble dry, Hand wash only, Do not iron, Do not bleach",
+                        "https://img01.ztat.net/article/P4/48/1L/01/JQ/11/P4481L01J-Q11@13.jpg?imwidth=156",
                         new List<string>
                         {
-                            "https://images-na.ssl-images-amazon.com/images/I/81hLKdZYpAL._AC_UY1000_.jpg",
-                            "https://images-na.ssl-images-amazon.com/images/I/81hLKdZYpAL._AC_UY1000_.jpg",
-                            "https://images-na.ssl-images-amazon.com/images/I/81hLKdZYpAL._AC_UY1000_.jpg",
-                            "https://images-na.ssl-images-amazon.com/images/I/81hLKdZYpAL._AC_UY1000_.jpg",
+                            "https://img01.ztat.net/article/P4/48/1L/01/JQ/11/P4481L01J-Q11@17.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/P4/48/1L/01/JQ/11/P4481L01J-Q11@15.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/P4/48/1L/01/JQ/11/P4481L01J-Q11@16.jpg?imwidth=156",
                         },
                         19.89m,
                         20,
@@ -98,14 +96,14 @@
                         1
                     ),
                     (
-                        "Womens Sexy Prashchishta New Collection (2020)",
-                        "Luftfilter, Ölfilter, Zündkerzen - die Marke *Champion* gehört zu den ganz großen der Szene.",
-                        "https://cdn.shopify.com/s/files/1/2424/0679/products/386377FYB_OM_B_1024x1024.jpg?v=1553278833",
+                        "Protest ALE - Bikini",
+                        "Material & care Outer fabric material: 80% polyamide, 20% elastane Bottom part material: 80% polyamide, 20% elastane Filling: 100% polyester Lining: 100% polyester Care instructions: Do not tumble dry, Hand wash only, Do not iron, Do not bleach",
+                        "https://img01.ztat.net/article/L8/38/1A/00/7Q/11/L8381A007-Q11@13.jpg?imwidth=156",
                         new List<string>
                         {
-                            "https://cdn.shopify.com/s/files/1/2424/0679/products/386377FYB_OM_B_1024x1024.jpg?v=1553278833",
-                            "https://cdn.shopify.com/s/files/1/2424/0679/products/386377FYB_OM_B_1024x1024.jpg?v=1553278833",
-                            "https://cdn.shopify.com/s/files/1/2424/0679/products/386377FYB_OM_B_1024x1024.jpg?v=1553278833",
+                            "https://img01.ztat.net/article/L8/38/1A/00/7Q/11/L8381A007-Q11@12.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/L8/38/1A/00/7Q/11/L8381A007-Q11@11.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/L8/38/1A/00/7Q/11/L8381A007-Q11@10.jpg?imwidth=156",
                         },
                         119.89m,
                         20,
@@ -113,14 +111,14 @@
                         0
                     ),
                     (
-                        "Mens Everlast Shoes",
-                        "Luftfilter, Ölfilter, Zündkerzen - die Marke *Champion* gehört zu den ganz großen der Szene.",
-                        "https://ae01.alicdn.com/kf/H465ff30618374811bdbcdd7c1f89a9d1b/EVERLAST-Spring-Summer-man-s-casual-sports-shoes-Fahion-patchwork-ventilated-man-s-casual-shoes.jpg",
+                        "Hunkemöller WILLOW - Underwired bra",
+                        "Material & care Outer fabric material: 67% polyamide, 22% polyester, 11% elastane Fabric: Lace Care instructions: Hand wash only",
+                        "https://img01.ztat.net/article/HM/18/1A/1F/JJ/11/HM181A1FJ-J11@13.jpg?imwidth=156",
                         new List<string>
                         {
-                            "https://ae01.alicdn.com/kf/H465ff30618374811bdbcdd7c1f89a9d1b/EVERLAST-Spring-Summer-man-s-casual-sports-shoes-Fahion-patchwork-ventilated-man-s-casual-shoes.jpg",
-                            "https://ae01.alicdn.com/kf/H465ff30618374811bdbcdd7c1f89a9d1b/EVERLAST-Spring-Summer-man-s-casual-sports-shoes-Fahion-patchwork-ventilated-man-s-casual-shoes.jpg",
-                            "https://ae01.alicdn.com/kf/H465ff30618374811bdbcdd7c1f89a9d1b/EVERLAST-Spring-Summer-man-s-casual-sports-shoes-Fahion-patchwork-ventilated-man-s-casual-shoes.jpg",
+                            "https://img01.ztat.net/article/HM/18/1A/1F/JJ/11/HM181A1FJ-J11@14.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/HM/18/1A/1F/JJ/11/HM181A1FJ-J11@17.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/HM/18/1A/1F/JJ/11/HM181A1FJ-J11@16.jpg?imwidth=156",
                         },
                         9.89m,
                         200,
@@ -128,15 +126,15 @@
                         10
                     ),
                     (
-                        "Mens Everlast Sweatshirt",
-                        "Luftfilter, Ölfilter, Zündkerzen - die Marke *Champion* gehört zu den ganz großen der Szene.",
-                        "https://images-na.ssl-images-amazon.com/images/I/61ayW7dnxrL._AC_UX679_.jpg",
+                        "LASCANA Push-up bra",
+                        "Material & care Outer fabric material: 100% silk Fabric: Lace Care instructions: Hand wash only",
+                        "https://img01.ztat.net/article/L8/38/1A/00/UQ/13/L8381A00U-Q13@9.jpg?imwidth=156",
                         new List<string>
                         {
-                            "https://images-na.ssl-images-amazon.com/images/I/61ayW7dnxrL._AC_UX679_.jpg",
-                            "https://images-na.ssl-images-amazon.com/images/I/61ayW7dnxrL._AC_UX679_.jpg",
-                            "https://images-na.ssl-images-amazon.com/images/I/61ayW7dnxrL._AC_UX679_.jpg",
-                            "https://images-na.ssl-images-amazon.com/images/I/61ayW7dnxrL._AC_UX679_.jpg",
+                            "https://img01.ztat.net/article/L8/38/1A/00/UQ/13/L8381A00U-Q13@10.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/L8/38/1A/00/UQ/13/L8381A00U-Q13@8.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/L8/38/1A/00/UQ/13/L8381A00U-Q13@6.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/L8/38/1A/00/UQ/13/L8381A00U-Q13@7.jpg?imwidth=156",
                         },
                         9.89m,
                         200,
@@ -144,15 +142,15 @@
                         110
                     ),
                     (
-                        "NoFear T-Shirt",
-                        "Luftfilter, Ölfilter, Zündkerzen - die Marke *Champion* gehört zu den ganz großen der Szene.",
-                        "https://ukstore.nofear.com/images/products/59855803_l.jpg",
+                        "Even&Odd BASIC - MINI A-LINE SKIRT - A-line skirt",
+                        "Material & care Outer fabric material: 95% viscose, 5% elastane Fabric: Jersey Care instructions: Do not tumble dry, machine wash at 30°C, Machine wash on gentle cycle",
+                        "https://img01.ztat.net/article/EV/42/1B/08/SQ/11/EV421B08S-Q11@7.jpg?imwidth=156",
                         new List<string>
                         {
-                            "https://ukstore.nofear.com/images/products/59855803_l.jpg",
-                            "https://ukstore.nofear.com/images/products/59855803_l.jpg",
-                            "https://ukstore.nofear.com/images/products/59855803_l.jpg",
-                            "https://ukstore.nofear.com/images/products/59855803_l.jpg",
+                            "https://img01.ztat.net/article/EV/42/1B/08/SQ/11/EV421B08S-Q11@4.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/EV/42/1B/08/SQ/11/EV421B08S-Q11@9.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/EV/42/1B/08/SQ/11/EV421B08S-Q11@6.jpg?imwidth=156",
+                            "https://img01.ztat.net/article/EV/42/1B/08/SQ/11/EV421B08S-Q11@8.jpg?imwidth=156&filter=packshot",
                         },
                         9.89m,
                         180,
